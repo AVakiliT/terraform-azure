3bbed9572909ab520959a58a3cf64ff71ca68525
Amir <amirvt92@gmail.com>
2023-02-28 21:13:36 -0500

    added resource group

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 variable "resource_group_name" {
   type = string
-  default = "ps-tfazure-vnet-main"
+  default = "ps-tfaz-vnet-main"
 }
 
 variable "location" {
@@ -13,9 +22,9 @@ variable "vnet_cidr_range" {
   default = ["10.0.0.0/16"]
 }
 
-variable "subnet_prefixed" {
+variable "subnet_prefixes" {
   type=list(string)
-  default = [ "10.0.0.0/24", "10.0.0.1/24" ]
+  default = [ "10.0.1.0/24", "10.0.2.0/24" ]
 }
 
 variable "subnet_names" {
@@ -24,17 +33,23 @@ variable "subnet_names" {
 }
 
 provider "azurerm" {
+  # subscription_id = var.subscription_id
   features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location
 }
 
 module "vnet-main" {
   source = "Azure/vnet/azurerm"
   version = "4.0.0"
   resource_group_name = var.resource_group_name
-  vnet_location = var.location
-  vnet_name = var.resource_group_name
+  vnet_location = azurerm_resource_group.rg.location
+  vnet_name = azurerm_resource_group.rg.name
   address_space = var.vnet_cidr_range
-  subnet_prefixes = var.subnet_names
+  subnet_prefixes = var.subnet_prefixes
   subnet_names = var.subnet_names
   nsg_ids = {}
   use_for_each = false
